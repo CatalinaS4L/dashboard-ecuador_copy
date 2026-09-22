e38668bb396700d696c0dad9a59c22fc237e4fbc
CatalinaS4L <catalina.lopez@s4learning.com>
2026-09-22 02:02:22 -0500

    Changed links for my normalized take of the copy
    Basically a copy of the copy to not f* it up

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 // 1. ENLACES DIRECTOS A GOOGLE SHEETS ECUADOR
 // ==============================================
 const MONTH_URLS = {
-  abril: `https://docs.google.com/spreadsheets/d/e/2PACX-1vQwIMNqHJXGVnh4-Bdyet4vnvKP1vSNTD0i46iq1xqFlkCejMoEbUETIn-qz6lV2cb5Y3hvQh0o01Ey/pub?gid=1186598423&single=true&output=csv`,
-  mayo: `https://docs.google.com/spreadsheets/d/e/2PACX-1vQwIMNqHJXGVnh4-Bdyet4vnvKP1vSNTD0i46iq1xqFlkCejMoEbUETIn-qz6lV2cb5Y3hvQh0o01Ey/pub?gid=931773328&single=true&output=csv`,
-  junio: `https://docs.google.com/spreadsheets/d/e/2PACX-1vQwIMNqHJXGVnh4-Bdyet4vnvKP1vSNTD0i46iq1xqFlkCejMoEbUETIn-qz6lV2cb5Y3hvQh0o01Ey/pub?gid=1230562695&single=true&output=csv`,
-  julio: `https://docs.google.com/spreadsheets/d/e/2PACX-1vQwIMNqHJXGVnh4-Bdyet4vnvKP1vSNTD0i46iq1xqFlkCejMoEbUETIn-qz6lV2cb5Y3hvQh0o01Ey/pub?gid=1034655502&single=true&output=csv`,
-  agosto: `https://docs.google.com/spreadsheets/d/e/2PACX-1vQwIMNqHJXGVnh4-Bdyet4vnvKP1vSNTD0i46iq1xqFlkCejMoEbUETIn-qz6lV2cb5Y3hvQh0o01Ey/pub?gid=833072153&single=true&output=csv`,
-  septiembre: `https://docs.google.com/spreadsheets/d/e/2PACX-1vQwIMNqHJXGVnh4-Bdyet4vnvKP1vSNTD0i46iq1xqFlkCejMoEbUETIn-qz6lV2cb5Y3hvQh0o01Ey/pub?gid=1748535358&single=true&output=csv`
+  febrero: `https://docs.google.com/spreadsheets/d/e/2PACX-1vSkQSOQez8QahZZaVSPTXSTJZSNi8DC3mGSEL4AAdlzIpj169XVFi_AuVpUJ5uQJpmZ4weWLyAPzazh/pub?gid=0&single=true&output=csv`,
+  marzo: `https://docs.google.com/spreadsheets/d/e/2PACX-1vSkQSOQez8QahZZaVSPTXSTJZSNi8DC3mGSEL4AAdlzIpj169XVFi_AuVpUJ5uQJpmZ4weWLyAPzazh/pub?gid=972956934&single=true&output=csv`,
+  abril: `https://docs.google.com/spreadsheets/d/e/2PACX-1vSkQSOQez8QahZZaVSPTXSTJZSNi8DC3mGSEL4AAdlzIpj169XVFi_AuVpUJ5uQJpmZ4weWLyAPzazh/pub?gid=1186598423&single=true&output=csv`,
+  mayo: `https://docs.google.com/spreadsheets/d/e/2PACX-1vSkQSOQez8QahZZaVSPTXSTJZSNi8DC3mGSEL4AAdlzIpj169XVFi_AuVpUJ5uQJpmZ4weWLyAPzazh/pub?gid=931773328&single=true&output=csv`,
+  junio: `https://docs.google.com/spreadsheets/d/e/2PACX-1vSkQSOQez8QahZZaVSPTXSTJZSNi8DC3mGSEL4AAdlzIpj169XVFi_AuVpUJ5uQJpmZ4weWLyAPzazh/pub?gid=1230562695&single=true&output=csv`,
+  julio: `https://docs.google.com/spreadsheets/d/e/2PACX-1vSkQSOQez8QahZZaVSPTXSTJZSNi8DC3mGSEL4AAdlzIpj169XVFi_AuVpUJ5uQJpmZ4weWLyAPzazh/pub?gid=1034655502&single=true&output=csv`,
+  agosto: `https://docs.google.com/spreadsheets/d/e/2PACX-1vSkQSOQez8QahZZaVSPTXSTJZSNi8DC3mGSEL4AAdlzIpj169XVFi_AuVpUJ5uQJpmZ4weWLyAPzazh/pub?gid=833072153&single=true&output=csv`,
+  septiembre: `https://docs.google.com/spreadsheets/d/e/2PACX-1vSkQSOQez8QahZZaVSPTXSTJZSNi8DC3mGSEL4AAdlzIpj169XVFi_AuVpUJ5uQJpmZ4weWLyAPzazh/pub?gid=1748535358&single=true&output=csv`
 };
 
 const SUPERVISOR_COLORS = [
